7d82bd9d2efc102b3eafdd26a9a65a1ef9962ef0
Saurabh <saurabkumar679@gmail..com>
2024-11-05 08:37:45 +0530

    test

--- __tests__/components/ui/Input.test.tsx
+++ __tests__/components/ui/Input.test.tsx
@@ -1,16 +1,37 @@
-import {render, screen} from '@testing-library/react-native';
+import {fireEvent, render, screen} from '@testing-library/react-native';
 import Input from '../../../src/components/ui/Input';
 
 describe('Input element', () => {
-  test('Input component', () => {
-    render(<Input onFocus={() => {}} onBlur={() => {}} />);
-    const inputComp = screen.getByTestId('animatedView');
+  const mockOnChangeText = () => jest.fn();
+  const mockOnFocus = jest.fn();
+  const mockOnnBlur = jest.fn();
+
+  test('Input component should render correctly', () => {
+    render(
+      <Input
+        onChangeText={mockOnChangeText}
+        value="hajaj"
+        testID="inputComponent"
+      />,
+    );
+    const inputComp = screen.getByTestId('inputComponent');
     expect(inputComp).toBeTruthy();
   });
 
-  test('Input component', () => {
-    render(<Input error="error" />);
-    const inputComp = screen.getByTestId('animatedView');
-    expect(inputComp).toBeTruthy();
+  test('Input component shpuld focus and blur event', () => {
+    render(<Input error="error" onFocus={mockOnFocus} onBlur={mockOnnBlur} />);
+    const textInpComp = screen.getByTestId('textInput');
+    fireEvent(textInpComp, 'focus', {});
+    fireEvent(textInpComp, 'blur', {});
+    fireEvent(textInpComp, 'focus', {});
+    fireEvent(textInpComp, 'blur', {});
+    expect(mockOnFocus).toHaveBeenCalledTimes(2);
+    expect(mockOnnBlur).toHaveBeenCalledTimes(2);
+  });
+  test('Input component shpuld rener error', () => {
+    render(<Input error="error" error="error occurred" value="" />);
+    const errorTextInpComp = screen.getByTestId('errorText');
+
+    expect(errorTextInpComp).toBeTruthy();
   });
 });
